2b5aa4a7bc853fed3b7ec87bff4f2dfae54fc5a3
andpact@gmail.com <andpact@gmail.com>
2025-12-14 16:19:45 +0900

    feat: restructure categories and update navigation for improved organization

--- _posts/2025-12-14-first-post.md
+++ _posts/2025-12-14-first-post.md
@@ -2,6 +2,7 @@
 title: "첫 글"
 date: 2025-12-14
 layout: single
+categories: [git]
 ---
 
 이것이 첫 번째 게시물입니다.
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,19 +1,7 @@
 var store = [{
-        "title": "Android Studio",
-        "excerpt":" ","categories": [],
-        "tags": [],
-        "url": "/categories/androidstudio/",
-        "teaser": null
-      },{
-        "title": "Git",
-        "excerpt":" ","categories": [],
-        "tags": [],
-        "url": "/categories/git/",
-        "teaser": null
-      },{
         "title": "첫 글",
-        "excerpt":"이것이 첫 번째 게시물입니다.  ","categories": [],
+        "excerpt":"이것이 첫 번째 게시물입니다.  ","categories": ["git"],
         "tags": [],
-        "url": "/2025/12/14/first-post.html",
+        "url": "/git/2025/12/14/first-post.html",
         "teaser": null
       }]
