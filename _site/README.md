d77e20f68975a721b08bb10c31ff50b82fe2d52c
andpact@gmail.com <andpact@gmail.com>
2025-12-14 17:16:44 +0900

    Clean up local build artifacts and keep bundler config

--- _site/README.md
+++
@@ -1,1 +0,0 @@
-# andpact.github.io
--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "첫 글",
-        "excerpt":"이것이 첫 번째 게시물입니다.  ","categories": ["git"],
-        "tags": [],
-        "url": "/git/2025/12/14/first-post.html",
-        "teaser": null
-      }]
